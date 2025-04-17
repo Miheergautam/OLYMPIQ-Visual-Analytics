a68e59201c2c78903cf15f4ede2ac82146a582a4
Miheer Gautam <miheergautam04@gmail.com>
2025-04-18 03:24:19 +0530

    feat: education_expenditure routes and API done

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -2,9 +2,10 @@ from fastapi import APIRouter
 from .medal_routes import router as medal_router
 from .gdp_routes import router as gdp_router
 from .gdp_per_capita_routes import router as gdp_pc_router
+from .education_expenditure_routes import router as edu_router
 
 router = APIRouter()
 router.include_router(medal_router)
 router.include_router(gdp_router)
 router.include_router(gdp_pc_router)
-
+router.include_router(edu_router)
