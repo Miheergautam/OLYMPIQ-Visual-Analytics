5b55009def53174b75ca9d25b4cac5f635726604
Miheer Gautam <miheergautam04@gmail.com>
2025-04-18 03:18:33 +0530

    feat: gdp_per_capita routes and API done

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
 from .medal_routes import router as medal_router
 from .gdp_routes import router as gdp_router
+from .gdp_per_capita_routes import router as gdp_pc_router
 
 router = APIRouter()
 router.include_router(medal_router)
 router.include_router(gdp_router)
+router.include_router(gdp_pc_router)
+
