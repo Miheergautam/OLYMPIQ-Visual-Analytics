674cc459a32d31f1b0d5b18712a743fbb0fa5b30
Miheer Gautam <miheergautam04@gmail.com>
2025-04-20 23:10:49 +0530

    feat: urban_population_per api and routes added

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -4,6 +4,7 @@ from .gdp_routes import router as gdp_router
 from .gdp_per_capita_routes import router as gdp_pc_router
 from .education_expenditure_routes import router as edu_router
 from .political_stability_routes import router as political_stability_router
+from .urban_population_routes import router as urban_population_router
 
 router = APIRouter()
 router.include_router(medal_router)
@@ -11,3 +12,4 @@ router.include_router(gdp_router)
 router.include_router(gdp_pc_router)
 router.include_router(edu_router)
 router.include_router(political_stability_router)
+router.include_router(urban_population_router)
