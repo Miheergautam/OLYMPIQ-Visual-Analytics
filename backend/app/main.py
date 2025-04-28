1d0a26fec09e97884367eb1ceaa597b88c912de2
Miheer Gautam <miheergautam04@gmail.com>
2025-04-28 20:54:02 +0530

    feat: add clusteranalysis and dimentionality-red routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import router as api_router
+from app.dimensionality_reduction import router as dr_router
+from app.clusteranalysis import router as cluster_router
+
 
 app = FastAPI()
 
 # Add CORS settings
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Change to your frontend URL in production
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(api_router, prefix="/api")
+app.include_router(cluster_router, prefix="/api/clusteranalysis")
+app.include_router(dr_router, prefix="/api/dimensionality-reduction")
